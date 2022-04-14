391def137e79e73ba3922454e1a0ce76195c717c
Tahaj1234 <86033750+Tahaj1234@users.noreply.github.com>
2022-04-14 12:03:02 -0400

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,1 +1,35 @@
 
+import cv2
+import webcam_converter as wc
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k % 256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k % 256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        wc.convert_cam()
+        break
+
+cam.release()
+
+cv2.destroyAllWindows()
+
+
